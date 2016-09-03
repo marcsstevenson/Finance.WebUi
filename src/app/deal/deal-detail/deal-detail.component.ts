c04f9c3cde67c65b577c2df5bfd01d6a60e70f5e
Tim (Yu-xuan) Hong <hongyuxuan@gmail.com>
2016-09-03 19:33:15 +1200

    Deal detail page data binding, need more work on layout.

--- src/app/deal/deal-detail/deal-detail.component.ts
+++ src/app/deal/deal-detail/deal-detail.component.ts
@@ -1,16 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { DealService } from '../deal.service';
+// import { TinyEditor } from '../../shared/directives/tiny-editor/tiny-editor.directive';
+
+import { DealsData } from '../mockup-data';
+
+declare var tinymce: any;
 
 @Component({
   moduleId: module.id,
   selector: 'app-deal-detail',
   templateUrl: 'deal-detail.component.html',
-  styleUrls: ['deal-detail.component.css']
+  styleUrls: ['deal-detail.component.css'],
+  providers: [DealService]
 })
 export class DealDetailComponent implements OnInit {
 
-  constructor() { }
+  private deal: any;
+  public notes: Array<any>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private _dealService: DealService
+  ) { }
 
   ngOnInit() {
+
+    tinymce.init(
+      {
+        selector: "#test",
+      });
+
+    let dealId = this.route.snapshot.params['id'];
+    this.deal = this._dealService.getDeal(dealId)
+    .then((deal) => {
+      this.deal = deal;
+      console.log(this.deal);
+    })
+    .catch((err) => {
+      console.log(err); // dont do this, show the user a nice message
+    });
+  }
+
+  save() {
+    this._dealService.addOrSaveDeal(this.deal)
+    .then((response) => {
+      console.log('Saved successfully: ', response);
+      // this.router.navigateByUrl('/deal');
+    })
+    .catch((err) => {
+      console.log(err); // dont do this, show the user a nice message
+    });
+  }
+
+  delete() {
+    this._dealService.deleteDeal(this.deal.Id)
+    .then((response) => {
+      console.log('Deleted successfully: ', response);
+      this.router.navigateByUrl('/deal');
+    })
+    .catch((err) => {
+      console.log(err); // dont do this, show the user a nice message
+    });
   }
 
 }
