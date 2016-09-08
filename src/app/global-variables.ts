eac03f6e5d37760546c6273dafe01b26ebc5c240
Tim (Yu-xuan) Hong <hongyuxuan@gmail.com>
2016-09-09 09:32:49 +1200

    used api url

--- src/app/global-variables.ts
+++ src/app/global-variables.ts
@@ -2,8 +2,8 @@
 import * as moment from 'moment';
 
 export const GlobalVarables = Object.freeze({
-    // BASE_API_URL: 'http://financeplatform.azurewebsites.net/',
-    BASE_API_URL: 'http://192.168.1.233:1319/',
+    BASE_API_URL: 'http://financeplatform.azurewebsites.net/',
+    // BASE_API_URL: 'http://192.168.1.233:1319/',
     MIN_BIRTH_DATE: moment('1900-01-01').utc()
 });
 
