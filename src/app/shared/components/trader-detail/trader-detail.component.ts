74f5c977dee737385fc52bfd0d28a285831f8a1c
Tim (Yu-xuan) Hong <hongyuxuan@gmail.com>
2016-11-27 05:16:08 +1300

    trader detail update

--- src/app/shared/components/trader-detail/trader-detail.component.ts
+++ src/app/shared/components/trader-detail/trader-detail.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'fwui-trader-detail',
+  selector: 'fwui-vendor-detail',
   templateUrl: 'trader-detail.component.html',
   styleUrls: ['trader-detail.component.css']
 })
 export class FinanceWebUiTraderDetailComponent implements OnInit {
+
+  @Input()
+  vendorDetail;
+
+  @Output()
+  vendorDetailChange = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  update() {
+    console.log('I am sending update to parent');
+    this.vendorDetailChange.emit(
+      this.vendorDetail
+    );
+  }
+
+  updateInputProperty(property, $event) {
+    this.vendorDetail[property] = $event.target.value;
+    this.update();
+  }
 }
