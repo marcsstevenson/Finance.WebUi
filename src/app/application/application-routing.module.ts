a2909ad229913c4fc4e42dfc510ccd3242834634
Tim (Yu-xuan) Hong <hongyuxuan@gmail.com>
2017-02-28 23:16:10 +1300

    update route. some of the route is only temporary for local testing

--- src/app/application/application-routing.module.ts
+++ src/app/application/application-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { PersonalApplicationComponent } from './personal/personal-application.component';
 import { TransportationFormComponent } from './transportation-form/transportation-form.component';
 import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { MarineFormComponent } from './marine-form/marine-form.component';
+import { MotorcycleFormComponent } from './motorcycle-form/motorcycle-form.component';
 
 import { AuthorisationService } from '../shared/services/index';
 
@@ -36,6 +38,26 @@ const applicationRoutes: Routes = [
     {
         path: 'vehicle-form',
         component: VehicleFormComponent,
+        canActivate: [AuthorisationService]
+    },
+    {
+        path: 'application/:id/marine-form',
+        component: MarineFormComponent,
+        canActivate: [AuthorisationService]
+    },
+    {
+        path: 'marine-form',
+        component: MarineFormComponent,
+        canActivate: [AuthorisationService]
+    },
+    {
+        path: 'application/:id/motorcycle-form',
+        component: MotorcycleFormComponent,
+        canActivate: [AuthorisationService]
+    },
+    {
+        path: 'motorcycle-form',
+        component: MotorcycleFormComponent,
         canActivate: [AuthorisationService]
     }
 
